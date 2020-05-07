8a741dd0a3a652b6047d78869a0279b1369d3a80
csundman <csundman@gmail.com>
2020-05-07 14:54:46 -0700

    Update settings.lua
    Enable WOTG

--- scripts/globals/settings.lua
+++ scripts/globals/settings.lua
@@ -18,7 +18,7 @@ require("scripts/globals/common")
 -- Enable Expansion (1= yes 0= no)
 ENABLE_COP     = 1
 ENABLE_TOAU    = 1
-ENABLE_WOTG    = 0
+ENABLE_WOTG    = 1
 ENABLE_ACP     = 0
 ENABLE_AMK     = 0
 ENABLE_ASA     = 0
